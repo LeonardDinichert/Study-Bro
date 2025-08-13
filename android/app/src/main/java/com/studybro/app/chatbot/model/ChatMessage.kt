3fa77f3bdc44389e543402608d78c6f707b34658
Léonard Dinichert <84095132+LeonardDinichert@users.noreply.github.com>
2025-08-14 01:01:10 +0200

    Delete android directory

--- android/app/src/main/java/com/studybro/app/chatbot/model/ChatMessage.kt
+++
@@ -1,6 +0,0 @@
-package com.studybro.app.chatbot.model
-
-data class ChatMessage(
-    val role: String,
-    val content: String
-)
--- android/app/src/main/java/com/studybro/app/core/ui/theme/Type.kt
+++
@@ -1,5 +0,0 @@
-package com.studybro.app.core.ui.theme
-
-import androidx.compose.material3.Typography
-
-val Typography = Typography()
